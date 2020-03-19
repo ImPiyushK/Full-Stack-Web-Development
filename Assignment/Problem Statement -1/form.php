5886eb4421a266391cdc8f06786ec5e86f846fba
Piyush Kandwal <ultrapiyushkandwal@gmail.com>
2020-03-20 00:01:05 +0530

    Update form.php

--- Assignment/Problem Statement -1/form.php
+++ Assignment/Problem Statement -1/form.php
@@ -13,3 +13,40 @@
         echo "Welcome User";
     }
 ?>
+<html>
+    <head>
+        <title>
+        Insert Record
+        </title>
+    </head>
+
+    <body>
+        <form name="details" action="detail.php" method="post">
+            <table>
+                <tr>
+                <td>ID</td>
+                <td><input type="text" name="id" value=""></td>
+                </tr>
+                <tr>
+                    <td>Name</td>
+                    <td><input type="text" name="name" value=""></td>
+                </tr>
+                <tr>
+                    <td>age</td>
+                    <td><input type="text" name="age" value=""></td>
+                </tr>
+                <tr>
+                    <td>contact</td>
+                    <td><input type="text" name="contact" value=""></td>
+                </tr>
+                <tr>
+                    <td>email</td>
+                    <td><input type="text" name="email" value=""></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Submit" name='bttn'></td>
+                </tr>
+            </table>
+        </form> 
+    </body> 
+</html>
